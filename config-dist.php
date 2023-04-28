fa161e562b028e049b6882e92d59545dff4c95ae
hammertong <federico.montini@urmet.com>
2023-04-28 09:46:29 +0200

    removed password from git

--- config-dist.php
+++ config-dist.php
@@ -4,6 +4,6 @@
 define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "calendar");
 define("DB_CHARSET", "utf8mb4");
-define("DB_USER", "iotcadmin");
-define("DB_PASSWORD", "urM3t_Cl0ud_2014");
+define("DB_USER", "<very smart user ...>");
+define("DB_PASSWORD", "<very strong password...>");
 
