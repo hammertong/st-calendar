e6b7b8a5d4bbde97ab742db2831c99ec02cff45a
hammertong <federico.montini@urmet.com>
2023-04-28 17:30:15 +0200

    sql da sw excel

---
+++ sw_programmazione.sql
@@ -0,0 +1,60 @@
+
+
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-04-27 08:30:00', '2023-04-27 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-04 08:30:00', '2023-05-04 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-10 08:30:00', '2023-05-10 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-17 08:30:00', '2023-05-17 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-24 08:30:00', '2023-05-24 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-27 08:30:00', '2023-05-27 17:20:00', 'SW Federico Montini', '#ffffff', '#ffc000', userid);
+
+
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-04-28 08:30:00', '2023-04-28 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-05 08:30:00', '2023-05-05 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-11 08:30:00', '2023-05-11 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-18 08:30:00', '2023-05-18 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-25 08:30:00', '2023-05-25 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-28 08:30:00', '2023-05-28 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-31 08:30:00', '2023-05-31 17:20:00', 'SW Giuseppe Bruzzese', '#ffffff', '#ed7d31', userid);
+
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-04-27 08:30:00', '2023-04-27 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-04 08:30:00', '2023-05-04 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-11 08:30:00', '2023-05-11 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-18 08:30:00', '2023-05-18 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-25 08:30:00', '2023-05-25 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-27 08:30:00', '2023-05-27 17:20:00', 'SW Matteo Zuin', '#ffffff', '#ff0000', userid);
+
+
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-04-28 08:30:00', '2023-04-28 17:20:00', 'SW Simone Piazza', '#ffffff', '#70ad47', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-09 08:30:00', '2023-05-09 17:20:00', 'SW Simone Piazza', '#ffffff', '#70ad47', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-19 08:30:00', '2023-05-19 17:20:00', 'SW Simone Piazza', '#ffffff', '#70ad47', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-28 08:30:00', '2023-05-28 17:20:00', 'SW Simone Piazza', '#ffffff', '#70ad47', userid);
+INSERT INTO events (evt_start, evt_end, evt_text, evt_color, evt_bg, userid) VALUES 
+('2023-05-30 08:30:00', '2023-05-30 17:20:00', 'SW Simone Piazza', '#ffffff', '#70ad47', userid);
+
+
+
+
+
