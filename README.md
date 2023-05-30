28fc5e25d1cc7a608944baacf748fb9c2838e4b3
monti <federico.montini@urmet.com>
2023-05-30 11:45:49 +0200

    aggiunte note per la manutenzione

--- README.md
+++ README.md
@@ -16,5 +16,10 @@
   insert into profiles (roleid, userid) values (4, 6);
 
 ``
-
+## Fix colors, when updating default_color in users table
+``
+update events set evt_bg = (select default_color from users where users.userid = events.userid);
+``
+## List all admins
+select users.username, users.userid, `groups`.name, `groups`.id from profiles join users on profiles.userid = users.userid and roleid in (select id from roles where name like 'admin') join `groups` on users.grp = `groups`.id;
 
--- logout.php
+++ logout.php
@@ -1,19 +1,18 @@
 <?php
-
-
 session_destroy();
-//session_start();
-//unset($_SESSION['profile']);
-
-
-//header("Location: http://logout@calendar.local:8080");
-//header('WWW-Authenticate: Basic realm="realmName"');
-//header('HTTP/1.0 401 Unauthorized');
 ?>
 <p>Logging out ...</p>
 <script>
 
-document.execCommand("ClearAuthenticationCache");
-window.location = window.location.protocol + "//" + window.location.host + "/";
+//document.execCommand("ClearAuthenticationCache");
+var url = window.location.protocol + "//logout:logout@" + window.location.host + "/";
+window.location = url;
 
 </script>
+
+<div style="height: 24px; cursor: pointer; margin-left: 8px; margin-top: 2px; display: none;" onclick="logout()" title="logout">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <path fill='#ffffff' stroke='#ffffff' d="M3 3h8V1H3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8v-2H3z"/>
+        <path fill='#ffffff' stroke='#ffffff' d="M19 10l-6-5v4H5v2h8v4l6-5z"/>
+    </svg>
+</div>
